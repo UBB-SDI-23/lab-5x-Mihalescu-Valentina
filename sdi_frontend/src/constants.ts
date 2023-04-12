b928a20ce36f69c251465c13e5e1423734f37a7c
914-Mihalescu-Valentina <valentina.mihalescu@stud.ubbcluj.ro>
2023-04-12 12:31:46 +0300

    changed ip adress to fsega

--- sdi_frontend/src/constants.ts
+++ sdi_frontend/src/constants.ts
@@ -3,4 +3,4 @@
 //
 // export const BACKEND_API_URL =
 //     process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL
-export  const BACKEND_API_URL = "http://13.50.240.147/eurovision";
+export  const BACKEND_API_URL = "http://16.16.97.100/eurovision";
