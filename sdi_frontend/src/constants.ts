e5a7473cb497e667de4cc95625e31a2133dac330
914-Mihalescu-Valentina <valentina.mihalescu@stud.ubbcluj.ro>
2023-04-19 17:09:08 +0300

    changed ip adress to home Marginea

--- sdi_frontend/src/constants.ts
+++ sdi_frontend/src/constants.ts
@@ -1,6 +1,1 @@
-// const PROD_BACKEND_API_URL = "/eurovision";
-// const DEV_BACKEND_API_URL = "http://localhost:5173/eurovision";
-//
-// export const BACKEND_API_URL =
-//     process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL
-export  const BACKEND_API_URL = "http://16.16.97.100/eurovision";
+export  const BACKEND_API_URL = "http://13.50.239.120/eurovision";
