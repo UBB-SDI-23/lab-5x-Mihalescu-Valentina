b61d224709ade86dafa79143584e28cc7f44f069
914-Mihalescu-Valentina <valentina.mihalescu@stud.ubbcluj.ro>
2023-04-25 20:13:29 +0300

    changed the frontend

--- react_frontend/src/constants.ts
+++ react_frontend/src/constants.ts
@@ -1,4 +1,4 @@
 const DEV_API_URL = "http://127.0.0.1:8000/eurovision"
 const PROD_API_URL = "http://eurovision.csproject.org/eurovision"
 
-export  const BACKEND_API_URL = DEV_API_URL;
+export  const BACKEND_API_URL = PROD_API_URL;
