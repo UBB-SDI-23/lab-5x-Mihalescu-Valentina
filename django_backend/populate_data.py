d5d3a63a25184c05d83070a18192c0266818a80a
914-Mihalescu-Valentina <valentina.mihalescu@stud.ubbcluj.ro>
2023-04-26 15:51:05 +0300

    added script generator

--- django_backend/populate_data.py
+++ django_backend/populate_data.py
@@ -102,7 +102,7 @@ def hostcity_insert_data():
     for i in range(NO_RECORDS):
         host_city_name = fake.city()[:50]
         host_city_population = random.randint(1000,5000000)
-        host_city_mayor = fake.name.fullName()
+        host_city_mayor = fake.text(max_nb_chars=50)
         is_capital = bool(random.getrandbits(1))
         quality_factor = random.randint(10,10000)
         batch_values += f"('{host_city_name}', '{host_city_population}', '{host_city_mayor}', '{is_capital}', '{quality_factor}'),"
@@ -204,7 +204,7 @@ def artist_insert_data():
 
     for i in range(NO_RECORDS_INTERMEDIARY):
 
-        artist_name= fake.name.fullName()
+        artist_name= fake.text(max_nb_chars=50)
         artist_age = random.randint(1,125)
         description = fake.paragraph(nb_sentences=3)
         country = random.randint(1, NO_RECORDS)
