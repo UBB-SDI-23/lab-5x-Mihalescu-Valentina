e4f69c179c3dca114632bdfe5a184ae72405d71b
914-Mihalescu-Valentina <valentina.mihalescu@stud.ubbcluj.ro>
2023-04-26 08:01:17 +0300

    changed the constants

--- react_frontend/src/constants.ts
+++ react_frontend/src/constants.ts
@@ -1,4 +1,5 @@
 const DEV_API_URL = "http://127.0.0.1:8000/eurovision"
-const PROD_API_URL = "http://eurovision.csproject.org/eurovision"
+// const PROD_API_URL = "http://eurovision.csproject.org/eurovision"
+const PROD_API_URL = "http://16.16.149.235/eurovision"
 
 export  const BACKEND_API_URL = PROD_API_URL;
