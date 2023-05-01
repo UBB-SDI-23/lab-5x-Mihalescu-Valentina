36cb1e4e8f97dfac67a7e8df45c322bf3b8a16a8
914-Mihalescu-Valentina <valentina.mihalescu@stud.ubbcluj.ro>
2023-05-01 15:30:27 +0300

    added script generator

--- django_backend/populate_data.py
+++ django_backend/populate_data.py
@@ -14,6 +14,8 @@ fake = Faker()
 
 NO_RECORDS = 1000000
 NO_RECORDS_INTERMEDIARY = 10000000
+
+
 #
 
 
@@ -24,27 +26,33 @@ def drop_constraints_indexes():
 
     file.write("ALTER TABLE eurovision_venue DROP CONSTRAINT IF EXISTS eurovision_venue_pkey;\n")
 
-    file.write("ALTER TABLE eurovision_venue DROP CONSTRAINT IF EXISTS eurovision_venue_host_city_id_id_dc4c2aff_fk_eurovisio;\n")
+    file.write(
+        "ALTER TABLE eurovision_venue DROP CONSTRAINT IF EXISTS eurovision_venue_host_city_id_id_dc4c2aff_fk_eurovisio;\n")
 
     file.write("ALTER TABLE eurovision_edition DROP CONSTRAINT IF EXISTS eurovision_edition_pkey;\n")
 
-    file.write("ALTER TABLE eurovision_edition DROP CONSTRAINT IF EXISTS eurovision_edition_venue_id_id_8c70153c_fk_eurovision_venue_id;\n")
+    file.write(
+        "ALTER TABLE eurovision_edition DROP CONSTRAINT IF EXISTS eurovision_edition_venue_id_id_8c70153c_fk_eurovision_venue_id;\n")
 
     file.write("ALTER TABLE eurovision_country DROP CONSTRAINT IF EXISTS eurovision_country_pkey;\n")
 
     file.write("ALTER TABLE eurovision_artist DROP CONSTRAINT IF EXISTS eurovision_artist_pkey;\n")
 
-    file.write("ALTER TABLE eurovision_artist DROP CONSTRAINT IF EXISTS eurovision_artist_country_id_47605989_fk_eurovision_country_id;\n")
+    file.write(
+        "ALTER TABLE eurovision_artist DROP CONSTRAINT IF EXISTS eurovision_artist_country_id_47605989_fk_eurovision_country_id;\n")
 
     file.write("ALTER TABLE eurovision_song DROP CONSTRAINT IF EXISTS eurovision_song_pkey;\n")
 
-    file.write("ALTER TABLE eurovision_song DROP CONSTRAINT IF EXISTS eurovision_song_artist_id_id_865d0e06_fk_eurovision_artist_id;\n")
+    file.write(
+        "ALTER TABLE eurovision_song DROP CONSTRAINT IF EXISTS eurovision_song_artist_id_id_865d0e06_fk_eurovision_artist_id;\n")
 
     file.write("ALTER TABLE eurovision_ids DROP CONSTRAINT IF EXISTS eurovision_ids_pkey;\n")
 
-    file.write("ALTER TABLE eurovision_ids DROP CONSTRAINT IF EXISTS eurovision_ids_country_id_c0d299f5_fk_eurovision_country_id;\n")
+    file.write(
+        "ALTER TABLE eurovision_ids DROP CONSTRAINT IF EXISTS eurovision_ids_country_id_c0d299f5_fk_eurovision_country_id;\n")
 
-
+    file.write(
+        "ALTER TABLE eurovision_ids DROP CONSTRAINT IF EXISTS FK_eurovision_ids_edition_id;\n")
 
     file.close()
 
@@ -52,40 +60,43 @@ def drop_constraints_indexes():
 def add_constraints_indexes():
     file = open("add_constraints_indexes.sql", "w")
 
-    #file.write("ALTER TABLE eurovision_hostcity ADD CONSTRAINT eurovision_hostcity_pkey;\n")
+    # file.write("ALTER TABLE eurovision_hostcity ADD CONSTRAINT eurovision_hostcity_pkey;\n")
 
-    #file.write("ALTER TABLE eurovision_venue ADD CONSTRAINT eurovision_venue_pkey;\n")
+    # file.write("ALTER TABLE eurovision_venue ADD CONSTRAINT eurovision_venue_pkey;\n")
 
-    file.write("ALTER TABLE eurovision_venue ADD CONSTRAINT eurovision_venue_host_city_id_id_dc4c2aff_fk_eurovisio;\n")
+    file.write("ALTER TABLE eurovision_venue ADD CONSTRAINT eurovision_venue_host_city_id_id_dc4c2aff_fk_eurovisio FOREIGN key(hostcity_id_id) references eurovision_hostcity(id) on delete cascade;\n")
 
-    #file.write("ALTER TABLE eurovision_edition ADD CONSTRAINT eurovision_edition_pkey;\n")
-
-    file.write(
-        "ALTER TABLE eurovision_edition ADD CONSTRAINT eurovision_edition_venue_id_id_8c70153c_fk_eurovision_venue_id;\n")
-
-    #file.write("ALTER TABLE eurovision_country ADD CONSTRAINT eurovision_country_pkey;\n")
-
-    #file.write("ALTER TABLE eurovision_artist ADD CONSTRAINT eurovision_artist_pkey;\n")
+    # file.write("ALTER TABLE eurovision_edition ADD CONSTRAINT eurovision_edition_pkey;\n")
 
     file.write(
-        "ALTER TABLE eurovision_artist ADD CONSTRAINT  eurovision_artist_country_id_47605989_fk_eurovision_country_id;\n")
+        "ALTER TABLE eurovision_edition ADD CONSTRAINT eurovision_edition_venue_id_id_8c70153c_fk_eurovision_venue_id FOREIGN key(venue_id_id) references eurovision_venue(id) on delete cascade;\n")
 
-    file.write("ALTER TABLE eurovision_song ADD CONSTRAINT eurovision_song_pkey;\n")
+    # file.write("ALTER TABLE eurovision_country ADD CONSTRAINT eurovision_country_pkey;\n")
 
-    file.write(
-        "ALTER TABLE eurovision_song ADD CONSTRAINT  eurovision_song_artist_id_id_865d0e06_fk_eurovision_artist_id;\n")
-
-    file.write("ALTER TABLE eurovision_ids ADD CONSTRAINT eurovision_ids_pkey;\n")
+    # file.write("ALTER TABLE eurovision_artist ADD CONSTRAINT eurovision_artist_pkey;\n")
 
     file.write(
-        "ALTER TABLE eurovision_ids ADD CONSTRAINT  eurovision_ids_country_id_c0d299f5_fk_eurovision_country_id;\n")
+        "ALTER TABLE eurovision_artist ADD CONSTRAINT  eurovision_artist_country_id_47605989_fk_eurovision_country_id FOREIGN key(country_id) references eurovision_country(id) on delete cascade;\n")
 
-    file.write("CREATE INDEX IDX_HostCity_ID ON eurovision_venue(host_city_id);\n")
-    file.write("CREATE INDEX IDX_Venue_ID ON eurovision_edition(venue_id);\n")
-    file.write("CREATE INDEX IDX_Country_ID ON eurovision_artist(country);\n")
-    file.write("CREATE INDEX IDX_Artist_ID ON eurovision_song(artist_id);\n")
-    file.write("CREATE INDEX IDX_Edition_IDD ON eurovision_ids(edition);\n")
-    file.write("CREATE INDEX IDX_Country_IDD ON eurovision_ids(country);\n")
+    # file.write("ALTER TABLE eurovision_song ADD CONSTRAINT eurovision_song_pkey;\n")
+
+    file.write(
+        "ALTER TABLE eurovision_song ADD CONSTRAINT  eurovision_song_artist_id_id_865d0e06_fk_eurovision_artist_id FOREIGN key(artist_id_id) references eurovision_artist(id) on delete cascade;\n")
+
+    # file.write("ALTER TABLE eurovision_ids ADD CONSTRAINT eurovision_ids_pkey;\n")
+    #
+    file.write(
+        "ALTER TABLE eurovision_ids ADD CONSTRAINT  eurovision_ids_country_id_c0d299f5_fk_eurovision_country_id FOREIGN key(country_id) references eurovision_country(id) on delete cascade;\n")
+
+    file.write(
+        "ALTER TABLE eurovision_ids ADD CONSTRAINT  FK_eurovision_ids_edition_id FOREIGN key(edition_id) references eurovision_edition(id) on delete cascade;\n")
+
+    file.write("CREATE INDEX IDX_HostCity_ID ON eurovision_venue(host_city_id_id);\n")
+    file.write("CREATE INDEX IDX_Venue_ID ON eurovision_edition(venue_id_id);\n")
+    file.write("CREATE INDEX IDX_Country_ID ON eurovision_artist(country_id);\n")
+    file.write("CREATE INDEX IDX_Artist_ID ON eurovision_song(artist_id_id);\n")
+    file.write("CREATE INDEX IDX_Edition_IDD ON eurovision_ids(edition_id);\n")
+    file.write("CREATE INDEX IDX_Country_IDD ON eurovision_ids(country_id);\n")
 
     file.close()
 
@@ -100,10 +111,10 @@ def hostcity_insert_data():
 
     for i in range(NO_RECORDS):
         host_city_name = fake.city()[:50]
-        host_city_population = random.randint(1000,5000000)
+        host_city_population = random.randint(1000, 5000000)
         host_city_mayor = fake.text(max_nb_chars=50)
         is_capital = bool(random.getrandbits(1))
-        quality_factor = random.randint(10,10000)
+        quality_factor = random.randint(10, 10000)
         batch_values += f"('{host_city_name}', '{host_city_population}', '{host_city_mayor}', '{is_capital}', '{quality_factor}'),"
         if (i + 1) % 1000 == 0:
             file.write(
@@ -119,16 +130,14 @@ def venue_insert_data():
 
     batch_values = ""
 
-
-
     print("Generating SQL queries for inserting data in the Venue table...")
 
     for i in range(NO_RECORDS):
         nr = random.randint(1, 50)
-        venue_name = "Venue"+str(nr)
-        venue_adress = "Adress"+str(nr)
+        venue_name = "Venue" + str(nr)
+        venue_adress = "Adress" + str(nr)
         host_city_id_id = random.randint(1, NO_RECORDS)
-        capacity = random.randint(1,1000)
+        capacity = random.randint(1, 1000)
         rating = random.uniform(1.0, 5.0)
 
         batch_values += f"('{venue_name}', '{venue_adress}', {host_city_id_id},{capacity}, {rating}),"
@@ -149,11 +158,10 @@ def edition_insert_data():
     print("Generating SQL queries for inserting data in the Edition table...")
 
     for i in range(NO_RECORDS):
-        edition_year = random.randint(1960,2023)
+        edition_year = random.randint(1960, 2023)
         final_date = fake.date_between(datetime.date(1960, 1, 1), datetime.date(2023, 1, 1))
         motto = fake.text(max_nb_chars=50)
         venue_id_id = random.randint(1, NO_RECORDS)
-
 
         batch_values += f"('{edition_year}', '{final_date}', '{motto}', {venue_id_id}),"
         if (i + 1) % 1000 == 0:
@@ -173,16 +181,13 @@ def country_insert_data():
     print("Generating SQL queries for inserting data in the Country table...")
 
     for i in range(NO_RECORDS_INTERMEDIARY):
-        country_name= fake.country()[:50]
+        country_name = fake.country()[:50]
         country_name = country_name.replace("'", "")
-        year_of_entrance = random.randint(1960,2023)
+        year_of_entrance = random.randint(1960, 2023)
         country_capital = fake.city()[:50]
         nr = random.randint(1, 50)
         country_hymn = "Hymn" + str(nr)
-        quality_factor = random.randint(1,1000)
-
-
-
+        quality_factor = random.randint(1, 1000)
 
         batch_values += f"('{country_name}', '{year_of_entrance}', '{country_capital}', '{country_hymn}',{quality_factor}),"
 
@@ -204,13 +209,10 @@ def artist_insert_data():
 
     for i in range(NO_RECORDS_INTERMEDIARY):
 
-        artist_name= fake.text(max_nb_chars=50)
-        artist_age = random.randint(1,125)
+        artist_name = fake.text(max_nb_chars=50)
+        artist_age = random.randint(1, 125)
         description = fake.paragraph(nb_sentences=3)
         country_id = random.randint(1, NO_RECORDS)
-
-
-
 
         batch_values += f"('{artist_name}', '{artist_age}', '{description}', {country_id}),"
 
@@ -220,6 +222,7 @@ def artist_insert_data():
             batch_values = ""
 
     file.close()
+
 
 def song_insert_data():
     file = open("song_insert_data.sql", "w")
@@ -233,11 +236,7 @@ def song_insert_data():
         song_name = fake.text(max_nb_chars=50)
         artist_id_id = random.randint(1, NO_RECORDS)
         release_date = str(fake.date_between(datetime.date(1960, 1, 1), datetime.date(2023, 1, 1)))
-        album_name = "Album"+song_name
-
-
-
-
+        album_name = "Album" + song_name
 
         batch_values += f"('{song_name}', '{artist_id_id}', '{release_date}', '{album_name}'),"
 
@@ -259,10 +258,10 @@ def ids_insert_data():
 
     for i in range(NO_RECORDS_INTERMEDIARY):
 
-        country_id= random.randint(1, NO_RECORDS)
+        country_id = random.randint(1, NO_RECORDS)
         edition_id = random.randint(1, NO_RECORDS)
-        place = random.randint(1,30)
-        points = random.randint(1,1000)
+        place = random.randint(1, 30)
+        points = random.randint(1, 1000)
 
         batch_values += f"('{country_id}', '{edition_id}', {place}, {points}),"
 
@@ -275,12 +274,12 @@ def ids_insert_data():
 
 
 if __name__ == '__main__':
-    #drop_constraints_indexes()
-    #add_constraints_indexes()
-    #hostcity_insert_data()
-    #venue_insert_data()
-    #edition_insert_data()
-    #country_insert_data()
-    #artist_insert_data()
+    # drop_constraints_indexes()
+    # add_constraints_indexes()
+    # hostcity_insert_data()
+    # venue_insert_data()
+    # edition_insert_data()
+    # country_insert_data()
+    # artist_insert_data()
     song_insert_data()
-    #ids_insert_data()
+    # ids_insert_data()
