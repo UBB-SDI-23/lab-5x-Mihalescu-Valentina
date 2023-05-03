f2a20b1e74410446ee0ac8b6a303d900c99764ed
914-Mihalescu-Valentina <valentina.mihalescu@stud.ubbcluj.ro>
2023-05-03 14:52:47 +0300

    changed the frontend

--- django_backend/eurovision/serializers.py
+++ django_backend/eurovision/serializers.py
@@ -17,7 +17,7 @@ class HostCitySerializer(serializers.ModelSerializer):
 class VenueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Venue
-        fields = ('id', 'venue_name', 'venue_adress', 'capacity', 'rating', 'host_city_id')
+        fields = ('id', 'venue_name', 'venue_adress', 'capacity', 'rating', 'host_city_id','nb_editions')
 
 
 class EditionSerializer(serializers.ModelSerializer):
