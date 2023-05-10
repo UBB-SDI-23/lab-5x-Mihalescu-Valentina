3e76558f68474a13423d82793684e3994ff1fa00
914-Mihalescu-Valentina <valentina.mihalescu@stud.ubbcluj.ro>
2023-05-10 13:00:21 +0300

    the pagination was changed

--- react_frontend/src/components/Pagination.tsx
+++ react_frontend/src/components/Pagination.tsx
@@ -16,40 +16,6 @@ const Pagination = ({entitiesPerPage,totalPages,currentPage,paginate}:Props) => 
         pageNumbers.push(i);
     }
 
-    const getPageRange = () => {
-        const MAX_PAGES = 10; // maximum number of page links to show
-        const range = [];
-        let start = 1;
-        let end = totalPages;
-
-        // Calculate the start and end of the range based on the current page
-        if (totalPages > MAX_PAGES) {
-            if (currentPage <= 3) {
-                end = MAX_PAGES;
-            } else if (currentPage >= totalPages - 2) {
-                start = totalPages - MAX_PAGES + 1;
-            } else {
-                start = currentPage - 2;
-                end = currentPage + 2;
-            }
-        }
-
-        // Add page links for the range
-        for (let i = start; i <= end; i++) {
-            range.push(i);
-        }
-
-        // Add "..." links for the gaps between ranges
-        if (start > 1) {
-            range.unshift("...");
-        }
-        if (end < totalPages) {
-            range.push("...");
-        }
-
-        return range;
-    }
-
     // const getPageRange = () => {
     //     const MAX_PAGES = 10; // maximum number of page links to show
     //     const range = [];
@@ -75,20 +41,54 @@ const Pagination = ({entitiesPerPage,totalPages,currentPage,paginate}:Props) => 
     //
     //     // Add "..." links for the gaps between ranges
     //     if (start > 1) {
-    //         if (start > 6) {
-    //             range.unshift("...");
-    //         }
-    //         range.unshift(1);
+    //         range.unshift("...");
     //     }
     //     if (end < totalPages) {
-    //         if (end < totalPages - 5) {
-    //             range.push("...");
-    //         }
-    //         range.push(totalPages);
+    //         range.push("...");
     //     }
     //
     //     return range;
     // }
+
+    const getPageRange = () => {
+        const MAX_PAGES = 10; // maximum number of page links to show
+        const range = [];
+        let start = 1;
+        let end = totalPages;
+
+        // Calculate the start and end of the range based on the current page
+        if (totalPages > MAX_PAGES) {
+            if (currentPage <= 3) {
+                end = MAX_PAGES;
+            } else if (currentPage >= totalPages - 2) {
+                start = totalPages - MAX_PAGES + 1;
+            } else {
+                start = currentPage - 2;
+                end = currentPage + 2;
+            }
+        }
+
+        // Add page links for the range
+        for (let i = start; i <= end; i++) {
+            range.push(i);
+        }
+
+        // Add "..." links for the gaps between ranges
+        if (start > 1) {
+            if (start > 6) {
+                range.unshift("...");
+            }
+            range.unshift(1);
+        }
+        if (end < totalPages) {
+            if (end < totalPages - 5) {
+                range.push("...");
+            }
+            range.push(totalPages);
+        }
+
+        return range;
+    }
 
 
     return (
